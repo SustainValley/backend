2458dab844294469c9008bd68c7c53e79fd5c157
yujining3827 <j_yujin_swu22@naver.com>
2025-08-06 15:28:24 +0900

    fix : user entity 필드 수정

--- src/main/java/com/likelion/hackathon/entity/User.java
+++ src/main/java/com/likelion/hackathon/entity/User.java
@@ -9,16 +9,37 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // 로그인용 아이디 or 카카오고유번호 or 사업자번호
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = true)
     private String nickname;
 
     private String password;
-    private String provider; // "local", "google", "kakao"
+    private String provider; // "local", "kakao", "google"
 
-    private String email;
-    // private String role;     -- "ROLE_USER"
+    // 회원 타입 (일반회원: "pro", 사장님: "cor")
+    @Column(nullable = false)
+    private String type;
+
+    // ---- 사장님 전용 필드 ----
+    @Column(nullable = true)
+    private String businessnumber;
+
+    @Column(nullable = true)
+    private String presidentname;
+
+    @Column(nullable = true)
+    private String businessname;
+
+    @Column(nullable = true)
+    private String zipcode;
+
+    @Column(nullable = true)
+    private String address;
+
 }
 
 
