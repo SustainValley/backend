48a7533902527be6aab1c90c4db2049f669cd17c
yujining3827 <j_yujin_swu22@naver.com>
2025-08-06 15:51:54 +0900

    feat : SignupRequestDto에 사장님 정보 추가

--- src/main/java/com/likelion/hackathon/dto/UserDto/SignupRequestDto.java
+++ src/main/java/com/likelion/hackathon/dto/UserDto/SignupRequestDto.java
@@ -8,4 +8,11 @@ public class SignupRequestDto {
     private String username;
     private String password;
     private String provider;
+
+    // 사장님(cor) 회원가입 시 추가 필드
+    private String businessnumber;
+    private String presidentname;
+    private String businessname;
+    private String zipcode;
+    private String address;
 }
