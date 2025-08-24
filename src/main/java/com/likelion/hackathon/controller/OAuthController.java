58cbe3a7d91365b8e97ab002ab38cc599a554ce4
yujining3827 <j_yujin_swu22@naver.com>
2025-08-24 18:22:00 +0900

    feat : 카카오 로그인 분기처리 구현

--- src/main/java/com/likelion/hackathon/controller/OAuthController.java
+++ src/main/java/com/likelion/hackathon/controller/OAuthController.java
@@ -1,7 +1,12 @@
 package com.likelion.hackathon.controller;
 
 import com.likelion.hackathon.dto.UserDto.LoginResponseDto;
+import com.likelion.hackathon.dto.UserDto.LoginResponseWithPhoneDto;
+import com.likelion.hackathon.entity.User;
+import com.likelion.hackathon.repository.UserRepository;
 import com.likelion.hackathon.service.KakaoOAuthService;
+import com.likelion.hackathon.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuthController {
 
     private final KakaoOAuthService kakaoOAuthService;
+    private final UserRepository userRepository;
 
     @GetMapping("/kakao/callback")
+    @Operation(summary = "카카오 로그인", description = "기본 카카오 로그인")
     public ResponseEntity<LoginResponseDto> kakaoCallback(@RequestParam String code) {
         LoginResponseDto loginResponse = kakaoOAuthService.kakaoLogin(code);
         return ResponseEntity.ok(loginResponse);
     }
+
+    @GetMapping("/kakao/callback/2")
+    @Operation(summary = "카카오 로그인 (전화번호 여부 포함)", description = "카카오 로그인 후 전화번호 등록 여부를 함께 반환합니다.")
+    public ResponseEntity<LoginResponseWithPhoneDto> kakaoCallbackWithPhone(@RequestParam String code) {
+        LoginResponseDto loginResponse = kakaoOAuthService.kakaoLogin(code);
+
+        User user = userRepository.findById(loginResponse.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        boolean hasPhoneNumber = (user.getPhoneNumber() != null && !user.getPhoneNumber().isEmpty());
+
+        LoginResponseWithPhoneDto response = new LoginResponseWithPhoneDto(
+                loginResponse,
+                hasPhoneNumber
+        );
+
+        return ResponseEntity.ok(response);
+    }
+
 }
